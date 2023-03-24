09e05b869245d5a6c27591d92feffed097ea05e7
Athenais Mezloy <athenais.mezloy@gmail.com>
2023-03-24 16:26:25 +0100

    24/03/23 Avancée avant insertion API

--- view/home.php
+++ view/home.php
@@ -1,14 +1,41 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Yonnha</title>
+    <link rel="stylesheet" href="../view/style/style.css">
 </head>
+
 <body>
-    <?php
+    <header>
+        <?php
         include 'navbar.php';
-    ?>
+        ?>
+    </header>
+    <section id="home-first">
+    <h1>La révolution des couleurs</h1>
+    <h2>Exposition sur les techniques de l'impressionnisme </h2>
+    <a href="">Découvrir</a>
+    </section>
+    <section id="home-second">
+        <h3>Tableau</h3>
+        <!-- slider gsap -->
+    </section>
+    <section id="home-third">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc eget lorem dolor sed viverra ipsum nunc aliquet. Tristique nulla aliquet enim tortor at auctor. Magnis dis parturient montes </p>
+        <a href="">Voir le dispositif intéractif</a>
+    </section>
+    <section id="home-fourth">
+        <!-- slider2 -->
+        <!-- slider3 artiste -->
+    </section>
+<?php 
+include 'footer.php'
+?>
+
 </body>
+
 </html>
--- view/navbar.php
+++ view/navbar.php
@@ -1,18 +1,20 @@
-<nav>
-        <ul>
-            <li><a href="index.php">Accueil</a></li>
-            <li><a href="index.php?tag=gallery">Galerie</a></li>
-            <li><a href="index.php?tag=reservation">Réservation</a></li>
-            <li><a href="index.php?tag=contact">Contact</a></li>
+<nav class="navbar container">
+    <img src="./images/logo.png" alt="Logo link home">
+        <ul class="nav-list">
+            <li class="nav-item"><a href="../index.php" class="nav-link">Accueil</a></li>
+            <li class="nav-item"><a href="../index.php?tag=gallery" class="nav-link">Galerie</a></li>
+            <li class="nav-item"><a href="../index.php?tag=reservation" class="nav-link">Réservation</a></li>
+            <li class="nav-item"><a href="../index.php?tag=contact" class="nav-link">Contact</a></li>
+            <div class="active"></div>
             <?php if (!isset($_SESSION['login'])):?>
-                <li><a href="index.php?tag=login">Login</a></li>
-                <li><a href="index.php?tag=signup">Sign up</a></li>
+                <li class="nav-item"><a href="../index.php?tag=login" class="nav-link">Login</a></li>
+                <li class="nav-item"><a href="../index.php?tag=signup" class="nav-link">Sign up</a></li>
             <?php endif;?>
             <?php if (isset($_SESSION['login'])):?>
                 <?php if ($_SESSION['is_admin'] == true):?>
-                    <li><a href="index.php?tag=gestion_panel">Panneau de gestion</a></li>
+                    <li class="nav-item"><a href="../index.php?tag=gestion_panel" class="nav-link">Panneau de gestion</a></li>
                 <?php endif; ?>
-                <li><a href="./controller/controller-user.php?session_login=logout">déconnexion</a></li>
+                <li class="nav-item"><a href="./controller/controller-user.php?session_login=logout" class="nav-link">déconnexion</a></li>
             <?php endif; ?>
         </ul>
 </nav>
