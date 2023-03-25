b5ee3e9fb70294a5c1a41353d1f854a5165f8092
Athenais Mezloy <athenais.mezloy@gmail.com>
2023-03-25 15:16:44 +0100

    25/03/2023 Style Narbar

--- view/navbar.php
+++ view/navbar.php
@@ -1,20 +1,22 @@
-<nav class="navbar container">
+<nav class="navbar">
     <img src="./images/logo.png" alt="Logo link home">
+    <div class="container">
         <ul class="nav-list">
             <li class="nav-item"><a href="./index.php" class="nav-link">Accueil</a></li>
             <li class="nav-item"><a href="./index.php?tag=gallery" class="nav-link">Galerie</a></li>
             <li class="nav-item"><a href="./index.php?tag=reservation" class="nav-link">Réservation</a></li>
             <li class="nav-item"><a href="./index.php?tag=contact" class="nav-link">Contact</a></li>
             <div class="active"></div>
-            <?php if (!isset($_SESSION['login'])):?>
+            <?php if (!isset($_SESSION['login'])) : ?>
                 <li class="nav-item"><a href="./index.php?tag=account&action=login" class="nav-link">Login</a></li>
-                <li class="nav-item"><a href="./index.php?tag=account&action=signup" class="nav-link">Sign up</a></li>
-            <?php endif;?>
-            <?php if (isset($_SESSION['login'])):?>
-                <?php if ($_SESSION['is_admin'] == true):?>
+                <li class="nav-item"><a href="./index.php?tag=account&action=signup" class="nav-link">Signup</a></li>
+            <?php endif; ?>
+            <?php if (isset($_SESSION['login'])) : ?>
+                <?php if ($_SESSION['is_admin'] == true) : ?>
                     <li class="nav-item"><a href="./index.php?tag=gestion_panel" class="nav-link">Panneau de gestion</a></li>
                 <?php endif; ?>
-                <li class="nav-item"><a href="./index.php?tag=account&action=logout" class="nav-link">déconnexion</a></li>
+                <li class="nav-item"><a href="./index.php?tag=account&action=logout" class="nav-link">Déconnexion</a></li>
             <?php endif; ?>
         </ul>
+    </div>
 </nav>
