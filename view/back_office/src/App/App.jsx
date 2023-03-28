2c98dd802aa80e62daee35fb8f4d058dbb682280
Lilian <lilianpeuron@gmail.com>
2023-03-29 01:39:06 +0200

    Back Office Stat

--- view/back_office/src/App/App.jsx
+++ view/back_office/src/App/App.jsx
@@ -1,9 +1,12 @@
 import './App.css';
 import { UserTable } from '../component/UserTable/UserTable';
+import Chart from '../component/Chart/Chart';
 
 export const App = () => {
   return (
     <div className="App">
+        <a href="./">Retour sur le site</a>
+        <Chart/>
         <UserTable/>
     </div>
   );
