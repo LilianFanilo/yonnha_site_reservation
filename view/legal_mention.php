b285d2514f364cd58f18eb2695dc5318258440a3
Athenais Mezloy <athenais.mezloy@gmail.com>
2023-03-30 00:06:08 +0200

    30/03/2023 - Ajout style Account

---
+++ view/legal_mention.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./view/style/style.css">
+    <script defer src="./view/js/app.js"></script>
+    <script defer src="./view/js/functions.js"></script>
+    <link rel="icon" href="./view/images/logo_yonnha.png" type="image/icon type">
+    <title>Dispositif Intéractif</title>
+</head>
+<body>
+    
+</body>
