a2be9ad2ab981e28c2576e5fe3c8a0d76899ea3c
Lilian <lilianpeuron@gmail.com>
2023-03-30 02:51:03 +0200

    footer design

--- view/footer.php
+++ view/footer.php
@@ -1,9 +1,20 @@
 <footer>
     <div>
-        <h2></h2>
-        <a href="index.php?tag=legal_mention">Mentions Légales</a>
+        <div>
+            <img src="./view/images/logo_yonnha.png" alt="" class="logo">
+        </div>
+        <div class="container-footer">
+            <h2>À propos</h2>
+            <a href="https://yonnha.fr/">Notre agence</a>
+            <a href="index.php?tag=legal_mention">Mentions Légales</a>
+            <a href="">Données personnelles</a>
+        </div>
     </div>
-    <div>
-        <h2></h2>
+    <div class="container-footer">
+        <h2>Nous suivre</h2>
+        <div class="logo-container">
+            <img src="./view/images/insta.svg" alt="">
+            <img src="./view/images/tiktok.svg" alt="">
+        </div>
     </div>
 </footer>
